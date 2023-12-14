9b2b81f65c8d8e40fb9adc71e6234bed1ac98a08
unknown <sean.herbison@gmail.com>
2023-12-13 21:09:48 -0700

    Changes App to previously used template and adds Homepage page.

--- gifts-frontend/src/App.js
+++ gifts-frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// General Imports
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Page Imports
+import HomePage from "./pages/HomePage/HomePage";
+
+// Util Imports
+// import PrivateRoute from "./utils/PrivateRoute";
+// Add later, once I understand chain better
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
     </div>
   );
 }
