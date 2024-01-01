d5355a043098a7364c4e6545cfea458bcaa4ca2e
unknown <sean.herbison@gmail.com>
2024-01-01 16:51:52 -0700

    Adds links and changes style of NavBar.

--- gifts-frontend/src/components/NavBar/NavBar.jsx
+++ gifts-frontend/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
 
-import styles from "./NavBar.module.css";
-
-/**
- * The Navbar component provides a navigation bar at the top of the page.
- * It uses context to check if a user is logged in and adjusts the menu accordingly.
- * @returns {JSX.Element} The rendered JSX for the Navbar component.
- */
-const Navbar = () => {
-
+const NavBar = () => {
   return (
     <div className={styles.navBar}>
       <ul>
-        {/* Standard links that are always present at the top of the page*/}
-        <li className={styles.brand}>
-          <Link to="/">
-            <b>Home</b>
+        {/* Each list item <li> is a navigation link */}
+        <li className={styles.navItem}>
+          <Link to="/all">
+            View All
           </Link>
         </li>
-        <li className={styles.brand}>
-          <Link to="/">
-            <b>Exit</b>
+        <li className={styles.navItem}>
+          <Link to="/other">
+            View Other
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/self">
+            View Self
           </Link>
         </li>
       </ul>
@@ -29,4 +27,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default NavBar;
