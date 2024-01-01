368bcc77b82e68042f91f8a8ef7ebbbffe591fb5
unknown <sean.herbison@gmail.com>
2024-01-01 16:54:01 -0700

    Removes links from GiftListPage. Redundant with NavBar.

--- gifts-frontend/src/pages/GiftListPage.js
+++ gifts-frontend/src/pages/GiftListPage.js
@@ -1,8 +1,4 @@
 import React, { useState } from 'react';
-// import ViewAll from './ViewAll'; // Component for the "View All" tab content
-// import EditOther from './EditOther'; // Component for the "Edit Other" tab content
-// import EditSelf from './EditSelf'; // Component for the "Edit Self" tab content
-// import './GiftListPage.css';
 
 const GiftListPage = () => {
   const [activeTab, setActiveTab] = useState('all'); // State to track active tab
@@ -14,34 +10,9 @@ const GiftListPage = () => {
 
   return (
     <div>
-      <div className="tabs">
-        <button onClick={() => handleTabClick('all')}>View All</button>
-        <button onClick={() => handleTabClick('other')}>Edit Other</button>
-        <button onClick={() => handleTabClick('self')}>Edit Self</button>
-      </div>
-      <div className="tab-content">
-        {/* {activeTab === 'all' && <ViewAll />}
-        {activeTab === 'other' && <EditOther />}
-        {activeTab === 'self' && <EditSelf />} */}
-      </div>
+      <h1>Gifts</h1>
     </div>
   );
 };
 
 export default GiftListPage;
-
-  // /**
-  //  * Renders the component.
-  //  */
-  // return (
-  //   <div className="container">
-  //     <h1>Gifts</h1>
-  //     <div className="contentBox">
-  //       {gifts.map((gift) => (
-  //         <div key={gift.id}>
-  //             {gift.name}
-  //         </div>
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
