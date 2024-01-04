d29f16f35ca16a1da366fdd783d4aa20f565e44f
unknown <sean.herbison@gmail.com>
2024-01-04 10:49:39 -0700

    Adds input fields to add a new gift for someone else.

--- gifts-frontend/src/pages/ViewOtherPage.js
+++ gifts-frontend/src/pages/ViewOtherPage.js
@@ -6,9 +6,46 @@ import "../App.css";
  * @component
  */
 const ViewOtherPage = () => {
+  // Render the form for file upload
   return (
     <div className="container">
-      <h1>Welcome to ViewOther</h1>
+      <h1>Upload Meet Results</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Gift Name:
+          <input type="text" name="itemName" required />
+        </label>
+        <label>
+          Yes:
+          <input type="radio" name="exactItem" value="yes" required />
+        </label>
+        <label>
+          No:
+          <input type="radio" name="exactItem" value="no" required />
+        </label>
+        <label>
+          Multiple Items:
+          <input type="radio" name="exactItem" value="yes" required />
+        </label>
+        <label>
+          Single item:
+          <input type="radio" name="exactItem" value="no" required />
+        </label>
+        {users.map((user, index) => (
+          <label key={index}>
+            {user.name}:
+            <input type="checkbox" name="user" value={user.id} />
+          </label>
+        ))}
+        <div className={styles.buttonContainer}>
+          <button type="submit">Add Gift</button>
+          <button
+            type="submit"
+            onClick={(e) => handleSubmit(e, true)}
+          >
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
