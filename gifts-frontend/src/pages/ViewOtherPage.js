42ba2a23cc22adb3e86bb22c5529b42147c5d2ee
unknown <sean.herbison@gmail.com>
2024-01-28 14:04:20 -0700

    Removes needless passing of member_id.

--- gifts-frontend/src/pages/ViewOtherPage.js
+++ gifts-frontend/src/pages/ViewOtherPage.js
@@ -34,7 +34,7 @@ const ViewOtherPage = () => {
         // Filter `allGifts` to only include gifts where `gift_receiver` matches `member.member_id`
         const memberGifts = allGifts.filter(gift => gift.gift_receiver === member.member_id);
         // Render the `GiftBox` component passing the `member` and their specific `gifts`
-        return <GiftBox key={member.member_id} member={member} gifts={memberGifts} />;
+        return <GiftBox member={member} gifts={memberGifts} />;
       })}
     </div>
   );
