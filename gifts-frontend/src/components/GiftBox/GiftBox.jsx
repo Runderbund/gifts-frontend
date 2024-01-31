8b760188e120cb604bb51c0656b35b9921c873f3
unknown <sean.herbison@gmail.com>
2024-01-31 10:01:05 -0700

    Adds a visibility column for the self view of giftbox.

--- gifts-frontend/src/components/GiftBox/GiftBox.jsx
+++ gifts-frontend/src/components/GiftBox/GiftBox.jsx
@@ -1,8 +1,13 @@
+import React, { useContext } from 'react';
+
 // The GiftBox component takes in two props: 
 // `member` - an object containing information about a member,
 // `gifts` - an array of gifts related to that member.
 // Creates a table structure for displaying gifts for each member
 const GiftBox = ({ member, gifts }) => {
+
+  const { selfMember } = useContext(MemberContext);
+
   return (
     <div>
       <h1>{member.member_name}'s Gifts</h1>
@@ -13,6 +18,8 @@ const GiftBox = ({ member, gifts }) => {
               <th>Exact?</th>
               <th>Multiple?</th>
               <th>Notes</th>
+              {selfMember && selfMember === member && <th>Visibility</th>}
+              {/* Only shows this column if the selfMember is the same as the member the giftBox is being called for. This should only be called from the selfView  */}
             </tr>
           </thead>
         <tbody>
@@ -22,6 +29,12 @@ const GiftBox = ({ member, gifts }) => {
               <td>{gift.exact_item ? 'Yes' : 'No'}</td>
               <td>{gift.multiple ? 'Yes' : 'No'}</td>
               <td>{gift.notes}</td>
+              {selfMember && selfMember === member && (
+                <td>
+                  {/* Render visibility options of all Members here. Probably checkboxes in add/edit, but just list here. Cap on width, probably. Maybe 4 names/row. 
+                  Default, and probably most common, will be All. */}
+                </td>
+              )}
             </tr>
           ))}
         </tbody>
