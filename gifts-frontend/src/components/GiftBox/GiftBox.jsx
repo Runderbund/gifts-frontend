63a92097b0d86690508193dd6fca390a78cc672b
unknown <sean.herbison@gmail.com>
2024-02-06 14:47:45 -0700

    The issue came back. I don't understand it, but it's working. For now, at least.

--- gifts-frontend/src/components/GiftBox/GiftBox.jsx
+++ gifts-frontend/src/components/GiftBox/GiftBox.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState} from 'react';
 import { MemberContext } from '../../context/MemberContext';
 import AddGift from '../AddGift/AddGift';
 
