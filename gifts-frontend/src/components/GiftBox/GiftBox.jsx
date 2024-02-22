eefcf90f785544634a8974ded264d30840bf7a67
unknown <sean.herbison@gmail.com>
2024-02-21 19:16:33 -0700

    Adds background and giftbox CSS styling.

--- gifts-frontend/src/components/GiftBox/GiftBox.jsx
+++ gifts-frontend/src/components/GiftBox/GiftBox.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState} from 'react';
 import { MemberContext } from '../../context/MemberContext';
 import AddGift from '../AddGift/AddGift';
+import "../../App.css";
 
 
 // The GiftBox component takes in two props: 
@@ -19,36 +20,38 @@ const GiftBox = ({ member, gifts }) => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>{member.member_name}'s Gifts</h1>
-      <table>
-        <thead>
-            <tr>
-              <th>Item</th>
-              <th>Exact?</th>
-              <th>Multiple?</th>
-              <th>Notes</th>
-              {isSelfMember && <th>Visibility</th>}
-              {/* Only shows this column if the selfMember is the same as the member the giftBox is being called for. This should only be called from the selfView  */}
-            </tr>
-          </thead>
-        <tbody>
-          {gifts.map((gift) => (
-            <tr key={gift.gift_id}>
-              <td>{gift.item_name}</td>
-              <td>{gift.exact_item ? 'Yes' : 'No'}</td>
-              <td>{gift.multiple ? 'Yes' : 'No'}</td>
-              <td>{gift.notes}</td>
-              {isSelfMember && (
-                <td>
-                  {/* Render visibility options of all Members here. Probably checkboxes in add/edit, but just list here. Cap on width, probably. Maybe 4 names/row. 
-                  Default, and probably most common, will be All. */}
-                </td>
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="giftBox">
+        <table>
+          <thead>
+              <tr>
+                <th>Item</th>
+                <th>Exact?</th>
+                <th>Multiple?</th>
+                <th>Notes</th>
+                {isSelfMember && <th>Visibility</th>}
+                {/* Only shows this column if the selfMember is the same as the member the giftBox is being called for. This should only be called from the selfView  */}
+              </tr>
+            </thead>
+          <tbody>
+            {gifts.map((gift) => (
+              <tr key={gift.gift_id}>
+                <td>{gift.item_name}</td>
+                <td>{gift.exact_item ? 'Yes' : 'No'}</td>
+                <td>{gift.multiple ? 'Yes' : 'No'}</td>
+                <td>{gift.notes}</td>
+                {isSelfMember && (
+                  <td>
+                    {/* Render visibility options of all Members here. Probably checkboxes in add/edit, but just list here. Cap on width, probably. Maybe 4 names/row. 
+                    Default, and probably most common, will be All. */}
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
       <button onClick={toggleAddGiftPopup}>Add Gift</button>
       {showAddGift && <AddGift member={member} isSelfMember={isSelfMember} closePopup={toggleAddGiftPopup} />}
     </div>
