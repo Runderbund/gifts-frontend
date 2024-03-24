b1485b33a4428ff2b9451c1572268beaa2857161
unknown <sean.herbison@gmail.com>
2024-03-24 08:54:37 -0700

    Moves gift processing logic from frontend to views.

--- gifts-frontend/src/pages/ViewSelfPage.js
+++ gifts-frontend/src/pages/ViewSelfPage.js
@@ -15,40 +15,16 @@ const ViewSelfPage = () => {
   const [allGifts, setAllGifts] = useState([]);
   const navigate = useNavigate();
 
-  const fetchAllGifts = async (selfMember) => {
+  const fetchAllGifts = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/get_all_gifts_self/');
-      const giftsData = response.data.gifts;
-
-      const selfGifts = giftsData.filter(gift => {
-        // Checks if the gift is for the selfMember and if the gift_id is unique
-        if (gift.gift_receiver === selfMember.member_id) {
-          return true;
-        }
-        return false;
-      })
-    
-    // Create an object to hold gifts by ID with their visibility
-      const giftsById = {};
-
-      selfGifts.forEach((gift) => {
-        const memberObject = allMembers.find(member => member.member_id === gift.visible_to);
-        const memberName = memberObject.member_name;
-
-        
-        if (!giftsById[gift.gift_id]) {
-          // If the gift hasn't been added yet, add it with the member who can see it
-          giftsById[gift.gift_id] = {
-            ...gift,
-            visible_to: [memberName],
-          };
-        } else {
-          // If it has been added, just push the new member into the `visible_to` array
-          giftsById[gift.gift_id].visible_to.push(memberName);
+      const response = await axios.get('http://localhost:8000/get_all_gifts_self/', {
+        params: {
+          member_id: selfMember.member_id // Using member_id from selfMember object
         }
       });
-
-      setAllGifts(Object.values(giftsById)); // Now this will be an array of objects, each object including a visible_to array
+      const giftsData = response.data.gifts;
+    
+      setAllGifts(Object.values(giftsData)); // Now this will be an array of objects, each object including a visible_to array
     } catch (error) {
       console.error('Error fetching all gifts: ', error);
     }
