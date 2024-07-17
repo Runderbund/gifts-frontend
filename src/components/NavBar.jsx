695e6679819dd8493f8cf947e449f50ad8ebd9b0
unknown <sean.herbison@gmail.com>
2024-07-17 14:48:04 -0700

    Updates Navbar image for specific user.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../App.css";
+import { MemberContext } from '../context/MemberContext'; 
 
 const NavBar = () => {
+  const [backgroundImage, setBackgroundImage] = useState('navBar');
+  const { selfMember } = useContext(MemberContext);
+
+  // This works to change for Sasha, but leaves if Sasha is chosen by mistake and a new member chosen.
+  useEffect(() => {
+    if (selfMember){
+      if (selfMember.member_id === 2) {
+        setBackgroundImage('navBarSasha');
+      }
+    }
+  }, [selfMember])
+  
   return (
-    <div className="navBar">
+    <div className={backgroundImage}>
+    {/* <div className={backgroundImage}> */}
       <ul>
         {/* Each list item <li> is a navigation link */}
         <li className="navItem">
